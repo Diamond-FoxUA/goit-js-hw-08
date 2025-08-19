0c9b9e28b60684c4957c4dfe74bd326cf5bfc2c1
Dmytro Farbun <dimofarb@gmail.com>
2025-08-19 21:51:04 +0300

    add markup with js

--- js/gallery.js
+++ js/gallery.js
@@ -63,3 +63,22 @@ const images = [
     description: 'Lighthouse Coast Sea',
   },
 ];
+
+const galleryEl = document.querySelector('.gallery');
+
+const markup = images.map(({ preview, original, description }) => {
+  return `
+    <li class="gallery-item">
+      <a class="gallery-link" href="${original}">
+        <img
+          class="gallery-image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>
+  `
+}).join("");
+
+galleryEl.insertAdjacentHTML("beforeend", markup);
